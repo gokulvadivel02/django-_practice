252688e88f6dc51dd6de0c36eb77d676f88c39e4
gokulvadivel02 <gokulvadivel002@gmail.com>
2025-05-19 20:34:34 +0530

    first commit

---
+++ blog/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+app_name = "blog"
+
+urlpatterns = [
+    path("", views.index, name='index'),
+    path("post/<int:post_id>", views.post, name='post'),
+    path("change_new_url", views.new_url_view, name='new_page_url'),
+    path("old_url", views.old_url, name='old_url'),
+] 
---
+++ blog/views.py
@@ -0,0 +1,40 @@
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from django.urls import reverse
+import logging
+from .models import Post
+
+# Create your views here.
+
+# posts=[
+#         {'id':1, 'title': 'post 1', 'content': 'content 1'},
+#         {'id':2, 'title': 'post 2', 'content': 'content 2'},
+#         {'id':3, 'title': 'post 3', 'content': 'content 3'},
+#         {'id':4, 'title': 'post 4', 'content': 'content 4'},
+#     ]
+
+# getting data from db
+posts=Post.objects.all()
+
+def index (request):
+    title = 'portfolio gokul'
+    return render(request, 'hi.html', {'title':title , 'posts':posts})
+
+def post (request , post_id):
+    # return render(request , 'hello.html' )
+
+    # static field
+    # post = next((item for item in posts if item['id'] == post_id ),None)
+
+    # getting post id 
+    post = Post.objects.get(pk=post_id)
+
+    logger = logging.getLogger('testing')
+    logger.debug(F'post variable is {post}')
+    return render(request , 'hello.html' , {'post':post})
+
+def old_url (request ) :
+    return redirect(reverse('blog:new_page_url'))
+
+def new_url_view (request ) :
+    return HttpResponse('i am in new url...') 
